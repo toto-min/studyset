8f85ce779a802bee860390ffd967c56461a33062
jeonghee <toto_min4016@naver.com>
2023-02-21 22:11:52 +0900

    first commit

---
+++ components/JsonView.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <div v-html="impview">
+        </div>
+    </div>
+</template>
+
+<script>
+import * as importview from "../assets/html/importview.json";
+export default {
+    name: 'JsonView',
+    data(){
+        return {
+            impview: importview.imphtml
+        }
+    }
+}
+</script>
+<style scoped></style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <JsonView></JsonView>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
